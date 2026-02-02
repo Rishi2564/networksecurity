0375d1df9fc6b12c6f6e79a00056200fb964dfaf
Rishi2564 <rishabhpradhan2005@gmail.com>
2026-02-02 18:24:12 +0530

    data ingestion completed

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+TARGET_COLUMN = "Result"
+
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+
+FILE_NAME: str = "phisingData.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+DATA_INGESTION_COLLECTION_NAME = "NetworkData"
+DATA_INGESTION_DATABASE_NAME = "NetworkSecurityDB"
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+FEATURE_STORE_FILE_NAME: str = "feature_store.csv"  # ✅ ADD THIS
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
---
+++ networksecurity/entity/artifact_entity.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    """
+    Data Ingestion Artifact Entity
+    """
+    trained_file_path: str
+    test_file_path: str
