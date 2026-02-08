26761fe281309afc9f23abc3a033acd6366c049a
Rishi2564 <rishabhpradhan2005@gmail.com>
2026-02-09 01:20:52 +0530

    data transformation implemented

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -19,4 +19,13 @@ class DataValidationArtifact:
     invalid_train_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    """
+    Data Transformation Artifact Entity
+    """
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+    transformed_object_file_path: str
     
